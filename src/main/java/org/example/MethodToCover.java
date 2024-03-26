bcbe76d4261c3b8bfdde34dbea627517af2dc62f
Daria Kornieva <darya.aleksandrovna95@gmail.com>
2024-03-26 23:50:21 +0200

    created first method which should be covered with unit tests

---
+++ src/main/java/org/example/MethodToCover.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class MethodToCover {
+
+    public int getNumber(int x, int y) {
+        int z = 0;
+        if(x < 0 && y < 10) {
+            z = x - y + 100;
+        } else {
+            z = x - y + 100;
+        }
+
+        if(y < 0 || z > 50) {
+            z = (z - y) / 2;
+        }
+        return x + y + z;
+    }
+    
+}
