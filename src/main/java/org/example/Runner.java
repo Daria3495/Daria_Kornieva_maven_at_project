89eba5544881686ae0b1c789db94228144acd2ae
Daria Kornieva <darya.aleksandrovna95@gmail.com>
2024-03-29 00:21:32 +0200

    proceed with task, added 7 combinations

--- src/main/java/org/example/Runner.java
+++ src/main/java/org/example/Runner.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Runner {
 
-    public static void test(int x, int y, int expected, String description) {
+    public static void calculateArguments(int x, int y, int expected, String description) {
         MethodToCover theClass = new MethodToCover();
         int actual = theClass.getNumber(x, y);
         if (actual == expected) {
@@ -14,7 +14,13 @@ public class Runner {
 
     public static void main(String[] args) {
 
-        test(-1,2,4,"X>0; Y>0");
+        calculateArguments(-1,2,48,"First and second If checked, x<0, y <10, z>50");
+        calculateArguments(-50, 4,0,"First If checked, both arguments false for second if, x<0, y <10, z<50");
+        calculateArguments(-6,22,41,"First If skipped and second If checked, x<0, y >10, z>50");
+        calculateArguments(-80,12,-60,"Both If are skipped, x<0, y >10, z<50");
+        calculateArguments(-4,-6,44,"Both If checked and z - skipped, x<0, y <0, z>50");
+        calculateArguments(-47,-12,-21,"Both If checked and z - skipped, x<0, y <0, z<50 - может исчерпывающе");
+        calculateArguments(7,-3,60,"First If skipped, second If checked and z - skipped, x>0, y <10, z>50");
     }
 
 }
